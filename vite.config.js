3b77c3ae52a3dbbb046e7909105a6c9765193ee2
Nicklas Bryntesson <nicklas.bryntesson@kan.se>
2024-01-23 22:27:34 +0100

    Update packages and add some test tooling and code formatting

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
-import postcssNesting from 'postcss-nesting'
-import autoprefixer from 'autoprefixer'
-
+import postcssNesting from 'postcss-nesting';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [
-        postcssNesting(),
-        autoprefixer()
-      ],
+      plugins: [postcssNesting(), autoprefixer()],
     },
+    sourceMap: true,
   },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'src/html'),
     }),
   ],
-})
+});
